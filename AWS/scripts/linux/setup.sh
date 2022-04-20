9e948acd4c79cafa0425f65d5c15c7123011874a
Saptarshi Sardar <saptarshi.sardar@outlook.com>
2022-04-21 01:34:22 +0530

    modified scripts

--- AWS/scripts/linux/setup.sh
+++ AWS/scripts/linux/setup.sh
@@ -2,7 +2,7 @@ aws cloudformation create-stack --stack-name ${APP_NAME}stack \
   --template-url $TEMPLATE_URL \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters ParameterKey=AppName,ParameterValue=$APP_NAME \
-    ParameterKey=DBRootPassword,ParameterValue=$env:DB_ROOT_PASSWORD \
+    ParameterKey=DBRootPassword,ParameterValue=$DB_ROOT_PASSWORD \
     ParameterKey=VPCID,ParameterValue=$VPC_ID \
     ParameterKey=Subnet1ID,ParameterValue=$SUBNET_ID_1 \
     ParameterKey=Subnet2ID,ParameterValue=$SUBNET_ID_2 \
