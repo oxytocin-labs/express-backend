783f96d6855f903853390a789c92dac40dc9a051
Sap-360 <saptarshi.sardar@oxytocin.is>
2022-07-23 19:08:00 +0530

    minor changes

--- AWS/scripts/linux/setup-names.sh
+++ AWS/scripts/linux/setup-names.sh
@@ -11,6 +11,6 @@ export DB_ROOT_PASSWORD=CHANGE_THIS_PASSWORD
 export DOMAIN_NAME=
 
 # Get this values from AWS. Use any two subnets for now.
-export VPC_ID=
-export SUBNET_ID_1=
-export SUBNET_ID_2=
+export VPC_ID=vpc-00bc59e7d31995433
+export SUBNET_ID_1=subnet-00be2e2f28cb4e907
+export SUBNET_ID_2=subnet-0e5659b03e4f8a067
